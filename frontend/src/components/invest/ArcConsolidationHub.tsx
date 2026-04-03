752c310de135170765ed382044198a443308776a
nohem-mg <nohem.monnet.gani@icloud.com>
2026-04-04 00:15:41 +0200

    Update ArcConsolidationHub.tsx

--- frontend/src/components/invest/ArcConsolidationHub.tsx
+++ frontend/src/components/invest/ArcConsolidationHub.tsx
@@ -2,7 +2,6 @@
 
 import Image from "next/image";
 import SegmentedProgress from "@/components/SegmentedProgress";
-import { useMemo } from "react";
 
 interface ArcConsolidationHubProps {
   totalBaseRevenue: number;
